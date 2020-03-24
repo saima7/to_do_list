7c8c79416cc3ef0f259d3ac239df395db0ebdad6
tul0n <sayeeda.saima@selise.ch>
2020-03-24 15:44:39 +0600

    todays commit

--- script/script.js
+++ script/script.js
@@ -14,7 +14,6 @@ var todo = {
 
 
   save: function () {
-  //  save the current data to local storage
     localStorage.list = JSON.stringify(todo.data);
     todo.list();
   },
@@ -22,13 +21,12 @@ var todo = {
 
 
   list: function () {
-
     var container = document.getElementById("todo-list");
     container.innerHTML = "";
 
    
     if (todo.data.length > 0) {
-      var row = "", el = "";
+      var row = "", element = "";
       for (var key in todo.data) {
     
         row = document.createElement("div");
@@ -36,54 +34,56 @@ var todo = {
         row.dataset.id = key;
 
 
-        el = document.createElement("div");
-        el.classList.add("item");
+        element = document.createElement("div");
+        element.classList.add("item");
         if (todo.data[key][1] == 1) {
-          el.classList.add("done");
+          element.classList.add("done");
         }
         if (todo.data[key][1] == 2) {
-          el.classList.add("cx");
+          element.classList.add("cancel");
         }
-        el.innerHTML = todo.data[key][0];
-        row.appendChild(el);
+        element.innerHTML = todo.data[key][0];
+        row.appendChild(element);
 
 
        // Add edit button
-       el = document.createElement("input");
-       el.setAttribute("type", "button");
-       el.value = "edit";
-       el.classList.add("bedit");
-       el.addEventListener("click", function () {
-         todo.status(this, 3);
+       element = document.createElement("input");
+       element.setAttribute("type", "button");
+       element.value = "edit";
+       element.classList.add("btnEdit");
+       element.addEventListener("click", function () {
+        element.setAttribute("contentEditable", "true");
+         todo.status(this, 0);
        });
-       row.appendChild(el);
+       row.appendChild(element);
 
 
 
         // Add cancel button
-        el = document.createElement("input");
-        el.setAttribute("type", "button");
-        el.value = "\u2716";
-        el.classList.add("bdel");
-        el.addEventListener("click", function () {
+        element = document.createElement("input");
+        element.setAttribute("type", "button");
+        element.value = "\u2716";
+        element.classList.add("bdel");
+        element.addEventListener("click", function () {
           todo.status(this, 2);
         });
-        row.appendChild(el);
+        row.appendChild(element);
 
         // Add done button
-        el = document.createElement("input");
-        el.setAttribute("type", "button");
-        el.value = "\u2714";
-        el.classList.add("bdone");
-        el.addEventListener("click", function () {
+        element = document.createElement("input");
+        element.setAttribute("type", "button");
+        element.value = "\u2714";
+        element.classList.add("bdone");
+        element.addEventListener("click", function () {
           todo.status(this, 1);
         });
-        row.appendChild(el);
+        row.appendChild(element);
 
         // Add row to list
         container.appendChild(row);
       }
     }
+   
   },
 
   add: function () {
@@ -96,10 +96,10 @@ var todo = {
     todo.save();
   },
 
-  status: function (el, stat) {
+  status: function (element, stat) {
   // todo.status() : update item status
 
-    var parent = el.parentElement;
+    var parent = element.parentElement;
     todo.data[parent.dataset.id][1] = stat;
     todo.save();
   },
@@ -121,17 +121,98 @@ var todo = {
     }
   },
   show: function (type) {
-    if (type == 0) {
-      todo.data=todo.data
-     
+
+   if (type == 1) {
+    
+    var container = document.getElementById("todo-list");
+    container.innerHTML = "";
+
+   
+    if (todo.data.length > 0) {
+      var row = "", element = "";
+      for (var key in todo.data) {
+    
+        row = document.createElement("div");
+        row.classList.add("clearfix");
+        row.dataset.id = key;
+
+        if (todo.data[key][1] == 1) {
+          element = document.createElement("div");
+          element.classList.add("item");
+          element.classList.add("done");
+          element.innerHTML = todo.data[key][0];
+          row.appendChild(element);
+        }
+       
+
+        // Add row to list
+        container.appendChild(row);
+      }
+    }
+      
     }
 
     else {
+      var container = document.getElementById("todo-list");
+      container.innerHTML = "";
+  
+     
+      if (todo.data.length > 0) {
+        var row = "", element = "";
+        for (var key in todo.data) {
       
+          row = document.createElement("div");
+          row.classList.add("clearfix");
+          row.dataset.id = key;
+  
+          if (todo.data[key][1] == 0) {
+            element = document.createElement("div");
+            element.classList.add("item");
+            element.innerHTML = todo.data[key][0];
+            row.appendChild(element);
+
+
+            // Add edit button
+            element = document.createElement("input");
+            element.setAttribute("type", "button");
+            element.value = "edit";
+            element.classList.add("btnEdit");
+            element.addEventListener("click", function () {
+            todo.status(this, 3);
+            });
+            row.appendChild(element);
+
+
+
+           // Add cancel button
+           element = document.createElement("input");
+           element.setAttribute("type", "button");
+           element.value = "\u2716";
+           element.classList.add("bdel");
+           element.addEventListener("click", function () {
+           todo.status(this, 2);
+           });
+           row.appendChild(element);
+
+           // Add done button
+           element = document.createElement("input");
+           element.setAttribute("type", "button");
+           element.value = "\u2714";
+           element.classList.add("bdone");
+           element.addEventListener("click", function () {
+           todo.status(this, 1);
+           });
+           row.appendChild(element);
+
+            
+          }
+          // Add row to list
+          container.appendChild(row);
+        }
+      }
     }
+
   }
-
-
 };
 
 
@@ -146,7 +227,7 @@ window.addEventListener("load", function () {
   document.getElementById("show_all").addEventListener("click", function () {
     todo.load();
   });
-  document.getElementById("show_done").addEventListener("click", function () {
+  document.getElementById("show_todo").addEventListener("click", function () {
     todo.show(0);
   });
   document.getElementById("show_done").addEventListener("click", function () {
